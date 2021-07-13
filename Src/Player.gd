54b978fd4fcb7a5b6ae03cc988f4662168bc5687
taariqmo <taariq.mo19@gmail.com>
2021-07-13 13:49:58 -0700

    lerp added

--- Src/Player.gd
+++ Src/Player.gd
@@ -7,13 +7,16 @@ extends KinematicBody2D
 var velocity := Vector2.ZERO
 export var walk_speed := 100
 export var jump_speed := 750
+export var traction := 0.05
 export var gravity := 2000
 export var coins := 0
+export var idle_threshold := 0.1
 
 func change_animation():
 	if is_on_floor():
 		if velocity.x == 0:
 			$AnimatedSprite.play("Idle")
+			print("Idle")
 		else:
 			$AnimatedSprite.play("Walk")
 			if velocity.x < 0:
@@ -35,14 +38,18 @@ func _physics_process(delta):
 		x += walk_speed
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y -= jump_speed
-	velocity.x = x
+	#velocity.x = x
+	#velocity.x += (x - velocity.x) * traction
+	velocity.x = lerp(velocity.x, x, traction)
 	velocity.y += gravity * delta
 	#position += velocity * delta
+	if abs(velocity.x) < idle_threshold:
+		velocity.x = 0
 	velocity = move_and_slide(velocity, Vector2.UP)
+	print(velocity.x)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(delta)
 	change_animation()
 
 func collect_coin():
